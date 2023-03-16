a179a3492eaaa340093e7c66c43c9b35f8567590
lazarowend <lazaroclans1@gmail.com>
2023-03-16 00:54:05 -0300

    atualizao js

--- js/main.js
+++ js/main.js
@@ -1,17 +1,12 @@
 var x = true;
 var slideAtual = 0;
 var ballAtual = 0
-const arrow_1 = $('#sobre-1 img');
-const arrow_2 = $('#sobre-2 img')
-const arrow_3 = $('#sobre-3 img')
-const arrow_4 = $('#sobre-4 img')
+const arrows = $('.sobre img');
 const balls = $('.ball');
 const slides = $('.slide');
 const next = $('.btn-left');
 const prev = $('.btn-right');
-const ball_0 = $('.one-ball');
-const ball_1 = $('.two-ball');
-const ball_2 = $('.three-ball');
+
 
 // função para fechar o menu mobile pela parte transparente do background
 $('nav .dark').click(function() {
@@ -80,83 +75,32 @@ setInterval(nextSlide, 5000); // roda o slide a cada 5 segundos
 next.click(nextSlide);
 prev.click(prevSlide)
 
+
 // funções para mover o slide pelas bolinhas
-ball_0.click(function() {
-    slideAtual = 0
-    showSlide(slideAtual)
-});
+balls.click(function() {
+    var x = $(this).attr('target')
+    console.log(x - 1)
+    showSlide(x - 1);
+})
 
-ball_1.click(function() {
-    slideAtual = 1
-    showSlide(slideAtual)
-});
 
-ball_2.click(function() {
-    slideAtual = 2
-    showSlide(slideAtual)
-});
-
-function arrow() {
-    if (this.className == 'img-seta-1 seta-footer') {
-        if (x) {
-            $('footer #sobre-1 a').css({
-                'display':'block',
-            })
-            arrow_1.css('transform','rotate(180deg)')
-            x = false
-        }else {
-            $('footer #sobre-1 a').css({
-                'display':'none'
-            })
-            arrow_1.css('transform','rotate(0deg)')
-            x = true
-        }
-    } else if (this.className == 'img-seta-2 seta-footer') {
-        if (x) {
-            $('footer #sobre-2 a').css({
-                'display':'block',
-            })
-            arrow_2.css('transform','rotate(180deg)')
-            x = false
-        }else {
-            $('footer #sobre-2 a').css({
-                'display':'none'
-            })
-            arrow_2.css('transform','rotate(0deg)')
-            x = true
-        }
-    } else if (this.className == 'img-seta-3 seta-footer') {
-        if (x) {
-            $('footer #sobre-3 a').css({
-                'display':'block',
-            })
-            arrow_3.css('transform','rotate(180deg)')
-            x = false
-        }else {
-            $('footer #sobre-3 a').css({
-                'display':'none'
-            })
-            arrow_3.css('transform','rotate(0deg)')
-            x = true
-        }
-    } else {
-        if (x) {
-            $('footer #sobre-4 a').css({
-                'display':'block',
-            })
-            arrow_4.css('transform','rotate(180deg)')
-            x = false
-        }else {
-            $('footer #sobre-4 a').css({
-                'display':'none'
-            })
-            arrow_4.css('transform','rotate(0deg)')
-            x = true
-        }
+function mostrarLink(i, sobre) {
+    if (x) {
+        $('footer .sobre-'+i+' a').css({
+            'display':'block',
+        })
+        sobre.css('transform','rotate(180deg)')
+        x = false
+    }else {
+        $('footer .sobre-'+i+' a').css({
+            'display':'none'
+        })
+        sobre.css('transform','rotate(0deg)')
+        x = true
     }
 }
 
-arrow_1.click(arrow)
-arrow_2.click(arrow)
-arrow_3.click(arrow)
-arrow_4.click(arrow)
+arrows.click(function() {
+    var x = $(this).attr('target')
+    mostrarLink(x, $(this))
+})
